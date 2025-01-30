2f96f91144c814af8cc7b8074cb46f5322a65705
Soham Tulsyan <soham.tulsyan_ug2023@ashoka.edu.in>
2025-01-30 17:40:41 +0530

    About page updated

---
+++ app/competitions/page.tsx
@@ -0,0 +1,12 @@
+// app/competitions/page.tsx
+import Card from "@/components/Card";
+export default function Competitions() {
+    return (
+        <div className="min-h-screen  bg-center bg-fixed flex justify-center items-center">
+        <Card title="Competitions">
+            <p className="text-white-700">
+            </p>
+        </Card>
+        </div>
+    );
+}
