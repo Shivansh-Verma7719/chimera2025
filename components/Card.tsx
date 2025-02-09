e7d5597b4994c71b914eafa3d03f968ddccdf1fe
SantriptaSharma <santripta@gmail.com>
2025-02-10 02:56:15 +0530

    fix margins on card

--- components/Card.tsx
+++ components/Card.tsx
@@ -8,7 +8,7 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ title, children }) => {
   return (
-    <div className="content w-full mx-4 md:w-[60%] text-white-900 shadow-lg rounded-xl p-8 relative mb-16">
+    <div className="content w-full mx-auto md:w-[60%] text-white-900 shadow-lg rounded-xl p-8 relative mb-16">
       <h1 className="text-3xl sm:text-4xl mb-2 font-iceland">{title}</h1>
       <div className="flex justify-start items-center mb-4">
         <div className="h-1 w-8 sm:w-12 bg-cyan-300 mr-2"></div>
